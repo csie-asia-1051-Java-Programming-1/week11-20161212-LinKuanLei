fa15cc54696285e0c8adcd983812f3fab9b45d29
LinKuanLei <103051089@live.asia.edu.tw>
2016-12-12 16:37:00 +0800

    Update and rename ex01.java to ex01_103051089.java

---
+++ src/ex/ex01_103051089.java
@@ -0,0 +1,56 @@
+package ex;
+/*
+ * Date: 2016/12/12
+ * Author: 103051089 林冠磊
+ */
+public class ex01_103051089 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n>=1&&n<=10){
+			int[] t = new int[n];
+			int m;
+			int k;
+			int[] count = new int[n];
+			for(int i = 0;i<n;i++){
+				t[i] = sc.nextInt();
+				m = sc.nextInt();
+				k = sc.nextInt();
+				int ke[] = new int[k];
+				for(int j = 0 ; j<ke.length;j++){
+					ke[j]=sc.nextInt();
+				}
+				int temp = 0;
+				for(int j = 0 ; j<ke.length;j++){
+					for(int l = 0 ; l<ke.length;l++){
+						if(ke[j]<ke[l]){
+							temp=ke[j];
+							ke[j]=ke[l];
+							ke[l]=temp;
+						}
+					}
+				}
+				count[i]= 0;
+				for(int j = 0 ; j<m ; j++){
+					count[i] += ke[j];
+				}				
+//				for(int j = 0 ; j<ke.length;j++){
+//					System.out.print(ke[j]+"\t");
+//				}
+			}
+			for(int i = 0 ; i <count.length;i++){
+				if(count[i] <=t[i]){
+					System.out.println(count[i]);
+				}else if(count[i] >t[i]){
+					System.out.println("No way");
+				}
+			}
+			
+		}else{
+			System.out.println("Error");
+		}
+		
+	}
+
+}
